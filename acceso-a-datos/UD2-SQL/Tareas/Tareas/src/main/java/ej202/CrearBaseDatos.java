1729df65e545c004bb6d691dab1af86d556cd224
Rodrigo Iglesias <iglesias.nieto.rodrigo.ald@gmail.com>
2023-12-12 19:59:51 +0100

    Inicio ejercicio 202

--- acceso-a-datos/UD2-SQL/Tareas/Tareas/src/main/java/ej202/CrearBaseDatos.java
+++ acceso-a-datos/UD2-SQL/Tareas/Tareas/src/main/java/ej202/CrearBaseDatos.java
@@ -1,4 +1,31 @@
 package ej202;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class CrearBaseDatos {
+    private String nombre;
+
+    public CrearBaseDatos(String nombre) {
+        this.nombre = nombre;
+        /*TODO: crear base de datos*/
+        try (Connection con = DriverManager.getConnection(Main.URL, Main.USER, Main.PASS);
+             Statement stmt = con.createStatement()) {
+            String sql = String.format("CREATE DATABASE %s", this.nombre);
+            stmt.executeUpdate(sql);
+            System.out.println("Se ha creado la base de datos: " + this.nombre);
+        } catch (SQLException e) {
+            System.err.println("No se ha creado la base de datos: " + this.nombre);
+        }
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
--- acceso-a-datos/UD2-SQL/Tareas/Tareas/src/main/java/ej202/Main.java
+++ acceso-a-datos/UD2-SQL/Tareas/Tareas/src/main/java/ej202/Main.java
@@ -1,7 +1,11 @@
 package ej202;
 
 public class Main {
+    public static final String URL = "jdbc:mysql://127.0.0.1:3306/";
+    public static final String USER = "root";
+    public static final String PASS = "abc123.";
     public static void main(String[] args) {
-
+        final String BD = "libros";
+        CrearBaseDatos crearBaseDatos = new CrearBaseDatos(BD);
     }
 }
