0f98af437247dff450bcdf20cb75ca88978c12a0
Rodrigo Iglesias <iglesias.nieto.rodrigo.ald@gmail.com>
2023-12-14 21:54:58 +0100

    Comprimido examen

--- android/UD1-tecnologias-desarrollo-movil/Examen/Iglesias_Nieto_Rodrigo_Examen/app/src/main/java/com/example/instagram/navigation/AppScreens.kt
+++
@@ -1,6 +0,0 @@
-package com.example.instagram.navigation
-
-sealed class AppScreens(val route: String) {
-    object LoginScreen: AppScreens("login_screen")
-    object FeedScreen: AppScreens("feed_screen")
-}
