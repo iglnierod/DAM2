30b42be0715cdd3af450e49cda3a69b826e67710
Rodrigo Iglesias <iglesias.nieto.rodrigo.ald@gmail.com>
2024-02-21 21:41:30 +0100

    21-02-2024

---
+++ interfaces/UD8-Pruebas/Proyecto/DecimalCalculator.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package calculator;
+
+/**
+ *
+ * @author fgf
+ */
+public class DecimalCalculator {
+
+    public double add(double operand1, double operand2) {
+        return operand1 + operand2;
+    }
+
+    public double subtract(double operand1, double operand2) {
+        return operand1 - operand2;
+    }
+
+    public double multiply (double operand1, double operand2) {
+        return operand1 * operand2;
+    }
+
+    public double divide(double operand1, double operand2) {
+        return operand1 / operand2;
+    }
+
+}
